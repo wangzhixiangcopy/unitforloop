8025e89874d9d33b378ea80adbef38fa1d729ea9
wangzhixiangcopy <wangzhixiangcopy@163.com>
2019-06-21 10:28:14 +0800

    oneclass

---
+++ js/one class.js
@@ -0,0 +1,33 @@
+// document.write('hello world!!!!!   varaiable')
+// console.log('ie   trident   GoogleChrome  blink    safari   webkit    FireFox   Gecko  opera  blink');
+// var a = 10;
+// var b = ++a -1 + a++;
+// document.write("a:"+a +"   " +"b:"+ b);
+// a++;
+
+// document.write(a);
+
+
+// var  a = 10;
+// document.write(a++);
+// var a = 123,
+//     b = 234;
+// var a = a + b,
+//     b = a - b ,
+//     a = a -b;
+// document.write("a:" + a + "<br>" + 'b:' + b);
+// var a = 123,
+//     b = 234,
+//     c = a ;
+//     a = b;
+//     b = c;
+//     console.log('a的值变成了：' + a + 'b的值变成了' + b);
+//     var a = 1 > 2;
+//     console.log(a);
+// var a = undefined || 3;
+// console.log(a);
+// var a = !'';
+// document.write(a);
+// var a  = true;
+// a = !a;
+// console.log(a);
